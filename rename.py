09482b90995e698fb84cd6de32e2de8d7ef87fbf
localhost <111120791+Giuseppe7887@users.noreply.github.com>
2024-09-17 12:44:35 +0200

    Update rename.py

--- rename.py
+++ rename.py
@@ -1,15 +1,36 @@
+#!/usr/bin/env python3
 import os
+from colorama import Fore
 
 
 files = os.listdir()
 
 
-i= 0
+this_file = "rename.py"
+
+def allow_write_mode():
+    try:
+        os.system("sudo chmod  755 ./*")
+        print(Fore.GREEN + " > WRITE PRIVILEGES OBTAINED",Fore.RESET)
+    except:
+        print(Fore.RED + " > Error while tying to get write privileges",Fore.RESET)
+        
+def rename_file(file):
+
+    try:
+        new_file = file.split("(")
+        name = file.split("(")[0].strip()
+        ext = os.path.splitext(new_file[1])[1].strip()
+        new_name = name + ext
+        os.rename(file,new_name)
+        print(Fore.YELLOW,f" > Renamed {file} into {new_name}",Fore.RESET)
+    except: 
+        print(Fore.RED,f" > Error while renaming {file}",Fore.RESET)
+    
+
+# per linux
+allow_write_mode()
+
 for file in files:
-    if file != "rename.py" and not os.path.isdir(file):
-        try:
-            x = int(file.split(" ")[-1].split(".")[0])
-            os.rename(file,f"{i}.png")
-        except:
-            pass
-    i+=1
+    if os.path.isfile(file) and file != "rename.py":
+    	rename_file(file)

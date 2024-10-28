ef4965fb490e2d0e45f8b134ea3b32a546be532c
localhost <111120791+Giuseppe7887@users.noreply.github.com>
2024-10-28 10:11:52 +0100

    Update llama_api_interface.py

--- llama_api_interface.py
+++ llama_api_interface.py
@@ -1,7 +1,7 @@
 from json import loads
 from requests import post
 from os import system, environ
-from subprocess import Popen,DEVNULL
+from subprocess import Popen, DEVNULL
 import sys
 
 # install ollama from: https://ollama.com/
@@ -19,21 +19,24 @@ MODEL: str = "llama3.1"
 spin = False
 
 environ["OLLAMA_HOST"] = HOST
-Popen(["ollama", "serve"],stdout=DEVNULL, stderr=DEVNULL)
-
+Popen(["ollama", "serve"], stdout=DEVNULL, stderr=DEVNULL)
 
 
 def main() -> None:
     global spin
+    print("Loading..")
+    post(f"{HOST}/api/generate", json={"model": MODEL, "prompt": "hi"}, stream=True)
+    system("cls" if sys.platform == "win32" else "clear")
+
     while True:
         prompt = input("> ")
         res = post(
             f"{HOST}/api/generate",
             json={"model": MODEL, "prompt": str(prompt).strip()},
-            stream=True
+            stream=True,
         )
         if res.status_code == 200:
-            prev = ""
+            prev = f"{MODEL}: "
             for chunk in res.iter_lines():
                 if chunk:
                     data = loads(chunk.decode("utf-8"))
@@ -43,7 +46,6 @@ def main() -> None:
                     prev += response
         else:
             print("Error:", res.status_code)
-        
 
 
 main()
